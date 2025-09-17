d1d63b769e8774f3eda7896ad216ee3060dbc421
Pengulll <BrawlhallaPityClan@gmail.com>
2025-09-17 01:24:43 -0700

    vite is blocking traffic

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import {defineConfig} from 'vite';
 export default defineConfig({
   envDir: '../',
   server: {
+    allowedHosts: '*.ngrok-free.app',
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
